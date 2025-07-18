6269aa1e1d21540675441e2ad2f3ac1ea7075f96
Jessy-code-maker <unoakuanusi@gmail.com>
2025-07-18 16:58:16 +0100

    comments added

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let Upskill_profile = {
-  fullName: { lastName: "Anusi", firstName: "Unoaku" },
-  age: "27",
-  best_hobbies: ["Reading", "Singing", "Surfing the Web"],
-  greet: function () {
-    //An arrow function is used to inherit lexical `this`
-    const name = () => `${this.fullName.firstName} ${this.fullName.lastName}`;
+    fullName: { lastName: "Anusi", firstName: "Unoaku" },
+    age: "27",
+    best_hobbies: ["Reading", "Singing", "Surfing the Web"],
 
-    console.log(
-      `Hi, my name is ${name()} and my 3 best hobbies are ${Upskill_profile.best_hobbies.join(", ")}.`
+
+  greet: function () {
+    //An arrow function is used to access the `this` context of the parent object
+    const name = () => `${this.fullName.firstName} ${this.fullName.lastName}`;
+     console.log(
+       `Hi, my name is ${name()} and my 3 best hobbies are ${Upskill_profile.best_hobbies.join(", ")}.`
     );
   }
 };
@@ -15,27 +16,28 @@ let Upskill_profile = {
 Upskill_profile.greet();              //Hi, my name is Unoaku Anusi and my 3 best hobbies are Reading, Singing, Surfing the Web.
 
 console.log(
-Upskill_profile.best_hobbies[1]      //Singing
-);
+Upskill_profile.best_hobbies[1]);     //Singing
 
-//Callback function
+
+
+//Callback function example with a learning scenario
 let Web_dev = {topic:["HTML", "CSS", "JAVASCRIPT"]};
+
 const learn = (topic_choice,Learning_done) => {
   setTimeout(() => {
-  console.log (`Learning ${Web_dev.topic[topic_choice]
-    } was chosen. Start classes immediately!`
-  )
+   console.log (`Learning ${Web_dev.topic[topic_choice]} was chosen. Start classes immediately!`);
   Learning_done();
-},
- 3000);
-}
+}, 3000);           // 3 seconds
+};
 
-//callback
+//callback function to be executed after the learning is done
 const Learning_done = () => {
   setTimeout(() => {
-    console.log("Finished the Lesson!")
-},
- 5000);
-}
+    console.log("Finished the Lesson!");
+}, 5000);          // 5 seconds
+};
 
 learn(2,Learning_done);
+
+//Output after 3 seconds: Learning JAVASCRIPT was chosen. Start classes immediately!
+//Output after 5 seconds: Finished the Lesson!
